0f9ebae09398b78147f88509891c12872fdf9eca
vittanala Gnana sree <vittanalagnanasree@gmail.com>
2026-07-17 10:46:57 +0530

    Update myChatbot.py

--- myChatbot.py
+++ myChatbot.py
@@ -1,9 +1,11 @@
 import streamlit as st
 from google import genai
 
-client = genai.Client(api_key="AIzaSy_your_actual_api_key_here")
+client = genai.Client(
+    api_key=st.secrets["GEMINI_API_KEY"]
+)
 
-st.title("sree Chatbot")
+st.title("Sree Chatbot")
 
 if "chat" not in st.session_state:
     st.session_state.chat = []
@@ -13,12 +15,16 @@ prompt = st.chat_input("Say something...")
 if prompt:
     st.session_state.chat.append(("user", prompt))
 
-    response = client.models.generate_content(
-        model="gemini-2.5-flash",
-        contents=prompt,
-    )
+    try:
+        response = client.models.generate_content(
+            model="gemini-2.5-flash",
+            contents=prompt
+        )
 
-    st.session_state.chat.append(("assistant", response.text))
+        st.session_state.chat.append(("assistant", response.text))
+
+    except Exception as e:
+        st.error(f"Error: {e}")
 
 for role, msg in st.session_state.chat:
     with st.chat_message(role):
